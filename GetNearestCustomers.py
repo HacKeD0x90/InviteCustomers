741231572f86a26f5b9b1403573ede010ec89d2a
HacKeD0x90 <eng_khaled_sakr@yahoo.com>
2019-05-11 05:02:38 +0400

    Add files via upload

---
+++ GetNearestCustomers.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+import json
+import sys
+from geopy.distance import great_circle
+
+
+
+####Global Varriables#####
+IntercomCoordinates= (53.339428 , -6.257664)
+customers=sys.argv[1]
+
+############### A class object to load the json data in to the class ###############
+class Customer(object):
+    def __init__(self, data):
+	    self.__dict__ = json.loads(data)
+
+
+
+################## Function to calculate the distance between Customer and Intercom ######################
+def CalculateNearestPath(latitude,longitude):
+	CustomerCoordinates=(latitude,longitude)
+	return great_circle(IntercomCoordinates, CustomerCoordinates).km
+
+
+################### Function to sort print Invited Customers ######################
+def sortAndPrint(InvID,AllCust):
+	for id in InvID:
+		for y in AllCust:
+			Inv=Customer(y)
+			if Inv.user_id==id:
+				print "Customer  " + Inv.name + " with UserID " + str(Inv.user_id) + " is within 100 KM " 
+			else:
+				pass
+
+
+
+
+######################## Function to read the file and make necessary operations to get a list of Invited Customers ###############
+def ReadFile(CustomersFile):
+
+	print "Reading File With Name " + customers + "\r\n"
+	count=0
+	Length=0
+	AllCustomers=[]
+	InvitedCustomersID=[]
+	InvitedCustomers=[]
+	f = open(customers, "r")
+	for x in f:
+  		AllCustomers.insert(Length,x)
+  		Length =Length+1 
+		IntercomCustomer = Customer(x)
+		DistanceFromIntercom = int(CalculateNearestPath(IntercomCustomer.latitude,IntercomCustomer.longitude))
+		if DistanceFromIntercom <= 100:
+			InvitedCustomersID.insert(count,IntercomCustomer.user_id)
+			InvitedCustomers.insert(count,x)
+			count=count+1
+	InvitedCustomersID= sorted(InvitedCustomersID)
+	sortAndPrint(InvitedCustomersID,AllCustomers)
+	
+		
+
+
+
+	
+			
+			
+
+			
+
+
+################### Main Function Starts by reading the Customers.json File################
+if __name__ == '__main__':
+	ReadFile(customers)
---
+++ README.md
@@ -0,0 +1,11 @@
+## Dependencies
+
+1. Python 2.7 (exist by default)
+2. sudo apt-get install python-pip
+3. sudo pip install geopy
+
+## How to run
+python GetNearestCustomer.py customers.json > output.txt
+
+## Screenshot
+![Intercom](http://sakr.gq/images/Intercom.png "Script in action")
